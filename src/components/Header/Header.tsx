470bba377501c997bf0659da89bce4303ac50fb6
Kovalskyi Semen <esceyp249>
2023-01-06 12:34:14 +0200

    header has fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,10 @@ import '../../styles/icon.scss';
 import { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { Page } from './HeaderTypes';
+import { Logo } from '../../images/icons/Logo';
+import { Menu } from '../../images/icons/Menu';
+import { Favourites } from '../../images/icons/Favourites';
+import { ShopBag } from '../../images/icons/ShopBag';
 
 export const Header: React.FC = () => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -12,117 +16,83 @@ export const Header: React.FC = () => {
     function handleResize() {
       setWidth(window.innerWidth);
     }
-    
-    window.addEventListener('resize', handleResize)
+
+    window.addEventListener('resize', handleResize);
 
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
     <header className="header">
-      <a href='/' className='header__main-link'>
-        <img
-          src={require('../../images/icons/Logo.svg').default}
-          alt="Shopping bag"
-          className="header__main-icon"
-        />
+      <a href="/" className="header__main-link">
+        <Logo />
       </a>
 
-      <nav className='nav'>
-        <ul className='nav__list'>
-          <li className='nav__item '>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item ">
             <a
               href="/"
-              className={classNames(
-                'nav__link',
-                {
-                  'is-active': isChoosen === Page.Home,
-                },
-              )}
+              className={classNames('nav__link', {
+                'is-active': isChoosen === Page.Home,
+              })}
               onClick={() => setIsChoosen(Page.Home)}
             >
-                Home
+              Home
             </a>
           </li>
-          <li className='nav__item'>
+          <li className="nav__item">
             <a
               href="/phones"
-              className={classNames(
-                'nav__link',
-                {
-                  'is-active': isChoosen === Page.Phones,
-                },
-              )}
+              className={classNames('nav__link', {
+                'is-active': isChoosen === Page.Phones,
+              })}
               onClick={() => setIsChoosen(Page.Phones)}
             >
-                Phones
+              Phones
             </a>
           </li>
-          <li className='nav__item'>
+          <li className="nav__item">
             <a
               href="/tablets"
-              className={classNames(
-                'nav__link',
-                {
-                  'is-active': isChoosen === Page.Tablets,
-                },
-              )}
+              className={classNames('nav__link', {
+                'is-active': isChoosen === Page.Tablets,
+              })}
               onClick={() => setIsChoosen(Page.Tablets)}
             >
-                Tablets
+              Tablets
             </a>
           </li>
-          <li className='nav__item'>
+          <li className="nav__item">
             <a
               href="/accessories"
-              className={classNames(
-                'nav__link',
-                {
-                  'is-active': isChoosen === Page.Accessories,
-                },
-              )}
+              className={classNames('nav__link', {
+                'is-active': isChoosen === Page.Accessories,
+              })}
               onClick={() => setIsChoosen(Page.Accessories)}
             >
-                Accessories
+              Accessories
             </a>
           </li>
         </ul>
       </nav>
 
-      {width > 639
-        ? (
-          <div className="header__container-left-side">
-            <button className='header header__left-side-icons'>
-              <img
-                src={require(
-                  '../../images/icons/Favourites.svg'
-                ).default}
-                alt="Heart Like"
-                className="header__icon"
-              />
-            </button>
-            <button className='header header__left-side-icons'>
-              <img
-                src={require(
-                  '../../images/icons/ShopBag.svg'
-                ).default}
-                alt="Shopping bag"
-                className="header__icon"
-              />
-            </button>
-          </div>
-        ) : (
-          <div className="header__container-left-side">
-            <button className='header header__left-side-icons'>
-              <img
-                src={require('../../images/icons/Menu.svg').default}
-                alt="Menu"
-                className="header__icon"
-              />
-            </button>
-          </div>
-        )
-      }
+      {width > 639 ? (
+        <div className="header__container-left-side">
+          <button type="submit" className="header header__left-side-icons">
+            <Favourites />
+          </button>
+          <button type="submit" className="header header__left-side-icons">
+            <ShopBag />
+          </button>
+        </div>
+      ) : (
+        <div className="header__container-left-side">
+          <button type="submit" className="header header__left-side-icons">
+            <Menu />
+          </button>
+        </div>
+      )}
     </header>
   );
-}
+};
