d8de8aa363a48fc0274f503f5457249f141c1b7c
Viktoriia Ovcharenko <viktoriiaovcharenko1415@gmail.com>
2023-01-06 12:01:06 +0200

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 export const App = () => {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
-}
+};
