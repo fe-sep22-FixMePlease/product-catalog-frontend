0f8a7adb43d9a1685d584163db19306ab125f798
Viktoriia Ovcharenko <viktoriiaovcharenko1415@gmail.com>
2023-01-10 19:50:41 +0200

    phones page and pagination

---
+++ src/api/phones.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line
+const API_URL = 'https://spiffy-chimera-5151e2.netlify.app/.netlify/functions/server';
+
+export async function getAllProducts() {
+  const response = await fetch(`${API_URL}/products`);
+
+  return response.json();
+}
+
+export async function getSortedProducts(
+  category: string,
+  sortBy: string,
+  page: number,
+  perPage: number,
+) {
+  const response = await fetch(`${API_URL}/products?page=${page}&perPage=${perPage}&sortBy=${sortBy}&category=${category}`);
+
+  return response.json();
+}
