fa34bfdd1061522aaa790bfc4ae1c96164e8e23b
spiritkyiv <baolt.king@gmail.com>
2023-01-25 13:13:02 +0200

    slider bar

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react';
+import { ProductSlider } from '../ProductSlider';
 import './HomePage.scss';
 import { Slider } from './Slider';
+import { getAllProducts } from '../../api/phones';
+import { Phone } from '../../types/Phone';
 
 export const HomePage: React.FC = () => {
+  const [phones, setPhones] = useState<Phone[]>([]);
+
+  const loadPhones = async () => {
+    const allFromServer = await getAllProducts();
+
+    setPhones(allFromServer);
+  };
+
+  useEffect(() => {
+    loadPhones();
+  }, []);
+
   return (
     <div className="home container">
       <h1 className="home__title">Welcome to Nice Gadgets store!</h1>
 
       <Slider />
+      <ProductSlider
+        title="Hot prices"
+        products={phones}
+      />
     </div>
   );
 };
---
+++ src/components/ProductSlider/ProductSlider.tsx
@@ -0,0 +1,63 @@
+import './ProductSlider.scss';
+import React from 'react';
+import { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { CardItem } from '../CardItem';
+import { Phone } from '../../types/Phone';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+interface Props {
+  title: string,
+  products: Phone[],
+}
+
+export const ProductSlider: React.FC<Props> = ({ title, products }) => {
+  return (
+    <Swiper
+      modules={[Navigation]}
+      spaceBetween={16}
+      slidesPerView={4}
+      className="swiper"
+      navigation
+      breakpoints={{
+
+        320: {
+          width: 320,
+          slidesPerView: 1,
+        },
+
+        640: {
+          width: 640,
+          slidesPerView: 2,
+        },
+
+        768: {
+          width: 768,
+          slidesPerView: 3,
+        },
+
+        1200: {
+          width: 1200,
+          slidesPerView: 4,
+        },
+      }}
+    >
+
+      <h2
+        slot="container-start"
+      >
+        {title}
+      </h2>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <SwiperSlide>
+            <CardItem phone={product} phones={products} />
+          </SwiperSlide>
+        </div>
+      ))}
+    </Swiper>
+  );
+};
